64406642a033dbd059886d098f4f1a11f4d665d0
Soraeus <ondrasoid05@seznam.cz>
2025-10-09 20:14:01 +0200

    audio

--- menu.gd
+++ menu.gd
@@ -1,5 +1,10 @@
 extends Control
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
 
+func _ready():
+	main_buttons.visible = true
+	settings.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://World.tscn")
@@ -7,7 +12,15 @@ func _on_start_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	print("Settings pressed")
-
+	main_buttons.visible = false
+	settings.visible = true
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+
+
+func _on_back_settings_pressed() -> void:
+	_ready()
