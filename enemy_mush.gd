032280fc6e22c085030ac9489732b4ee7759a4a4
Soraeus <ondrasoid05@seznam.cz>
2025-12-02 18:04:11 +0100

    úprava nepřítele

--- enemy_mush.gd
+++ enemy_mush.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 65
 var player_chase = false
 var player = null
 
@@ -8,7 +8,24 @@ func _physics_process(delta):
 	if player_chase:
 		move_and_collide(Vector2(0,0))
 	if player_chase:
-		position += (player.position - position)/speed
+	#Calculate direction vector
+		direction = player.position - position
+
+	#Get normalized direction (length = 1)
+		direction_normalized = direction.normalize()
+
+	#Move at constant speed
+		position += direction_normalized * speed * delta_time 
+
+		$AnimatedSprite2D.play("Run")
+		
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("Idle") 
+	
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
